daac690e63e643947b4622534d1764a70f9653dd
Ali-dev <alin89328@gmail.com>
2023-10-11 15:41:01 +0500

    feat: added delete file functionality in dashboard, dashboard display on page

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -11,7 +11,25 @@ import {trpc} from '@/app/_trpc/client'
 import UploadButton from './UploadButton'
 
 const Dashboard = () => {
+	const [currentlyDeletingFile, setCurrentlyDeletingFile] = useState<
+		string | null
+	>(null)
+
 	const {data: files, isLoading} = trpc.getUserFiles.useQuery()
+
+	const utils = trpc.useContext()
+
+	const {mutate: deleteFile} = trpc.deleteFile.useMutation({
+		onSuccess: () => {
+			utils.getUserFiles.invalidate()
+		},
+		onMutate({id}) {
+			setCurrentlyDeletingFile(id)
+		},
+		onSettled() {
+			setCurrentlyDeletingFile(null)
+		},
+	})
 
 	return (
 		<main className='mx-auto max-w-7xl md:p-10'>
@@ -59,10 +77,15 @@ const Dashboard = () => {
 										mocked
 									</div>
 									<Button
+										onClick={() => deleteFile({id: file.id})}
 										size='sm'
 										className='w-full'
 										variant='destructive'>
-										<Trash className='h-4 w-4' />
+										{currentlyDeletingFile === file.id ? (
+											<Loader2 className='h-4 w-4 animate-spin' />
+										) : (
+											<Trash className='h-4 w-4' />
+										)}
 									</Button>
 								</div>
 							</li>
